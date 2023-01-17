04246fdadfd4815ddcb7e9bb9c1694e75579d5d6
Dawit Hagos <dawhagos@gmail.com>
2023-01-17 01:27:17 -0800

    finish implementing UI

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+import { uploadFile } from "./api/uploadFile";
+import { API_URL } from "./api/config";
+import { getFiles } from "./api/getFiles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [file, setFile] = useState();
+  const [uploads, setUploads] = useState([]);
+  
+  function handleFileSelect(e) {
+    setFile(e.target.files[0]);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const newFile = await uploadFile(file);
+    setUploads([...uploads, newFile]);
+  }
+
+  useEffect(() => {
+    getFiles().then((files) => {
+      setUploads(files);
+      setIsAuthenticated(true);
+    });
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isAuthenticated ? (
+        <a href={`${API_URL}/auth/google`}>login</a>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="file">File</label>
+          <input onChange={handleFileSelect} id="file" type="file"></input>
+          <button>upload</button>
+
+          {uploads.map((upload) => (
+            <div key={upload._id}>
+              <a href={`${API_URL}/files/${upload.filename}`}>
+                {upload.filename}
+              </a>
+            </div>
+          ))}
+        </form>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
